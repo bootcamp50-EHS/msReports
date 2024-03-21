832cfe50c8cd5a519c211b57bf16260b6170116d
Enrique HS <e.programador.hs@gmail.com>
2024-03-21 14:13:58 -0500

    limpiando codigo de service

--- src/main/java/com/bootcamp/ehs/service/Report01Service.java
+++ src/main/java/com/bootcamp/ehs/service/Report01Service.java
@@ -16,7 +16,61 @@ public class Report01Service {
     private final WebClientAccount accountService;
     private final WebClientCredit creditService;
 
+    public Mono<CustomerDTO> getReportAccountByCustomer(String clienteId) {
+        return clientService.findCustomerById(clienteId)
+                .flatMap(this::createCustomerDTO)
+                .flatMap(this::addAccountDetailsToCustomer)
+                .flatMap(this::addCreditDetailsToCustomer)
+                .onErrorResume(IllegalArgumentException.class, ex -> Mono.empty());
+    }
 
+    private Mono<CustomerDTO> createCustomerDTO(CustomerInfoDTO customerInfoDTO) {
+        switch (customerInfoDTO.getTypeCustomer()) {
+            case "Personal":
+                return Mono.just(new CustomerPersonDTO(customerInfoDTO));
+            case "Empresarial":
+                return Mono.just(new CustomerBusinessDTO(customerInfoDTO));
+            default:
+                return Mono.error(new IllegalArgumentException("Tipo de cliente desconocido: " + customerInfoDTO.getTypeCustomer()));
+        }
+    }
+
+    private Mono<CustomerDTO> addAccountDetailsToCustomer(CustomerDTO cliente) {
+        return accountService.findAccountsByCustomer(cliente.getId())
+                .flatMap(this::createDetailAccountDTO)
+                .collectList()
+                .doOnNext(cliente::setAccountList)
+                .thenReturn(cliente);
+    }
+
+    private Mono<DetailAccountDTO> createDetailAccountDTO(AccountDTO cuenta) {
+        return productService.findProductById(cuenta.getProductId())
+                .map(producto -> {
+                    DetailAccountDTO detailAccount = new DetailAccountDTO();
+                    detailAccount.setProductName(producto.getNameAccount());
+                    detailAccount.setAccountNumber(cuenta.getAccountNumber());
+                    detailAccount.setAmount(cuenta.getAmount());
+                    return detailAccount;
+                });
+    }
+
+    private Mono<CustomerDTO> addCreditDetailsToCustomer(CustomerDTO cliente) {
+        return creditService.findCreditsByCustomer(cliente.getId())
+                .map(this::createDetailCreditDTO)
+                .collectList()
+                .doOnNext(cliente::setCreditList)
+                .thenReturn(cliente);
+    }
+
+    private DetailCreditDTO createDetailCreditDTO(CreditDTO credito) {
+        DetailCreditDTO detailCredit = new DetailCreditDTO();
+        detailCredit.setProductName(credito.getCreditType());
+        detailCredit.setAmount(credito.getAmount());
+        return detailCredit;
+    }
+
+
+    /*
     public Mono<CustomerDTO> getReportAccountByCustomer(String clienteId) {
         return clientService.findCustomerById(clienteId)
                 .flatMap(customerInfoDTO -> {
@@ -54,5 +108,5 @@ public class Report01Service {
 
                 )
                 .onErrorResume(IllegalArgumentException.class, ex -> Mono.empty());
-    }
+    }*/
 }
